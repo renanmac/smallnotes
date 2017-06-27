cae3177e6ea80b2e2403ccb1dcd47072b169fff9
Renan Machado <renanmacs@gmail.com>
2017-06-27 01:42:41 -0300

    Creating Menu Partial

--- app/models/users_note.rb
+++ app/models/users_note.rb
@@ -2,6 +2,6 @@ class UsersNote < ApplicationRecord
   belongs_to :user
   belongs_to :note
   
-  validate_uniqueness_of :user, scope: :note
-  validate_presence :user, :note
+  validates_uniqueness_of :user, scope: :note
+  validates_presence_of :user, :note
 end
