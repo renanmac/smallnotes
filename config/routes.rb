70f2611b6d7954222a028349aa3b8cbd475defda
Renan Machado <renanmacs@gmail.com>
2017-06-27 01:02:23 -0300

    Creating Menu Partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root :to => "notes#index"
+  
   resource :notes, only: [:create, :update, :index, :show, :destroy]
   
   post '/users/:id/add_note/:note_id', to: 'users#add_note'
