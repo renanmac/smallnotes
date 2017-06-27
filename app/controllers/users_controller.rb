2beef9eb25b96f4d3d0c87f5e629803d626eeca1
Renan Machado <renanmacs@gmail.com>
2017-06-26 23:52:05 -0300

    Creating menu partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @note = current_user.notes.find params[:note_id]
     @user.guest_notes << @note
 
-   # NoteMailer.new_note_permission(@note, @user).deliver_now
+    NoteMailer.new_note_permission(@note, @user).deliver_now
     render json: {message: "Compartilhamento bem sucedido!"}, status: :ok
   end
 
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @note = current_user.notes.find params[:note_id]
     @user.guest_notes.delete(@note.id)
 
-    #NoteMailer.permission_revoked(@note, @user).deliver_now
+    NoteMailer.permission_revoked(@note, @user).deliver_now
     render json: {message: "Permissão revogada com sucesso!"}, status: :ok
   end
   
